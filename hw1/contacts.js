01462ffcfefdfa8b4837b8c9f59386a6482bdc64
Illia Kononenko <ilyanes88@gmail.com>
2022-08-28 00:51:01 +0300

    last

--- hw1/contacts.js
+++ hw1/contacts.js
@@ -2,7 +2,8 @@ const fs = require("fs/promises");
 const { nanoid } = require("nanoid");
 const path = require("path");
 
-const contactsPath = path.resolve("db", "contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+// console.log(contactsPath);
 
 const updateContacts = async (contacts) => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
@@ -52,3 +53,14 @@ module.exports = {
   removeContact,
   addContact,
 };
+
+// const updateById = async (id, name, email, phone) => {
+//   const contacts = await listContacts();
+//   const idx = contacts.findIndex((item) => item.id === id);
+//   if (idx === -1) {
+//     return null;
+//   }
+//   contacts[idx] = { id, name, email, phone  };
+//   await updateContacts(contacts);
+//   return contacts[idx];
+// };
